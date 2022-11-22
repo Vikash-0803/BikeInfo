a6ef1336d46e04f2df7c2ab3ebb7f51f3e96e323
Vikash-0803 <vikashashok37@gmail.com>
2022-11-22 11:54:35 +0530

    Bike info task

---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,27 @@
+package org.bike;
+
+public class Ktm implements Bike {
+
+	@Override
+	public void cost() {
+		
+		System.out.println("This is Ktm Bike");
+		System.out.println("Cost = 1,00,000");
+//		super.cost();
+	}
+
+	@Override
+	public void speed() {
+		
+		System.out.println("Speed = 120kmph");
+//		super.speed();
+	}
+
+	public static void main(String[] args) {
+		
+		Ktm k = new Ktm();
+		k.cost();
+		k.speed();
+		
+	}
+}
